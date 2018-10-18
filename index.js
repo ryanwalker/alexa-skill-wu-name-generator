b72ecf4395f344b5080bb2ac919af93e00e49846
Ryan Walker <ryan.walker@infusionsoft.com>
2018-10-18 07:19:34 -0700

    Calls WuName generator

--- index.js
+++ index.js
@@ -1,51 +1,43 @@
 const axios = require('axios');
 const cheerio = require('cheerio')
 
-console.log('Trying...');
-axios.post(
-  'https://recordstore.com/wuname.pl',
-  'fname=Ryan&sname=Walker'
-)
-.then(function(response) {
-  var datums = response.data
-  const $ = cheerio.load(datums)
-  const wuName = $('span.newname').text();
-  console.log(wuName);
-})
-.catch(function (error) {
-  console.log(error);
+const firstName = process.argv[2];
+const lastName = process.argv[3];
 
-});
+retrieveWuName(firstName, lastName);
 
+function retrieveWuName(firstName, lastName, callback) {
 
+  const formData = 'fname=' + firstName + '&sname=' + lastName;
+
+  axios.post(
+    'https://recordstore.com/wuname.pl',
+    formData
+  )
+  .then(function(response) {
+    var datums = response.data
+    const $ = cheerio.load(datums)
+    const wuName = $('span.newname').text();
+    console.log(firstName + ' ' + lastName + ', your Wu Name is \'' + wuName + '\'');
+  })
+  .catch(function (error) {
+    console.log(error)
+    console.log('Sorry, I\'m having trouble retrieing your wu name.');
+  });
+}
 
 
 exports.handler = async (event) => {
     var firstName = event.firstName;
     var lastName = event.lastName;
 
-    var instance = axios.create({
-      baseURL: 'https://www.recordstore.com/',
-      timeout: 1000,
-      headers: {
-        'Content-Type' : 'application/x-www-form-urlencoded'
+    retrieveWuName(firstName, lastName,
+      function(message) {
+        const response = {
+          statusCode: 200,
+          body: '{ "message": "' + message + '" }'
+        };
+        return response;
       }
-    });
-
-    axios.post('wuname.pl',
-    'fname=Ryan&sname=Walker')
-    .then(function(response) {
-      console.log(response);
-    })
-    .catch(function (error) {
-
-    });
-
-    const response = {
-
-        statusCode: 200,
-        body: JSON.stringify('Ok, ' + firstName + ' ' + lastName +
-        '. I\'ll retrieve your Wu Name.')
-    };
-    return response;
+    );
 };
