b703edfae4c6ef6649cc9f8d845b2f7c9e936087
Ryan Walker <ryan.walker@infusionsoft.com>
2018-10-19 10:31:17 -0700

    Lambda sends back alexa response

--- index.js
+++ index.js
@@ -1,7 +1,48 @@
+/* eslint-disable  func-names */
+/* eslint-disable  no-console */
+/* eslint-disable  no-restricted-syntax */
+
 const axios = require('axios');
 const cheerio = require('cheerio')
+const Alexa = require('ask-sdk');
 
-exports.handler = async (event) => {
+const SKILL_NAME = 'Wu Name Generator';
+const FALLBACK_MESSAGE_DURING_GAME = `The ${SKILL_NAME} skill can't help you with that.  Try guessing a number between 0 and 100. `;
+const FALLBACK_REPROMPT_DURING_GAME = 'Please guess a number between 0 and 100.';
+const FALLBACK_MESSAGE_OUTSIDE_GAME = `The ${SKILL_NAME} skill can't help you with that.  It will come up with a number between 0 and 100 and you try to guess it by saying a number in that range. Would you like to play?`;
+const FALLBACK_REPROMPT_OUTSIDE_GAME = 'Say yes to start the game or no to quit.';
+
+const LaunchRequest = {
+  canHandle(handlerInput) {
+    return true; //handlerInput.requestEnvelope.request.type === 'LaunchRequest';
+  },
+  async handle(handlerInput) {
+    const responseBuilder = handlerInput.responseBuilder;
+    const speechOutput = `Welcome to ${SKILL_NAME}. Would you like to find out what your wu name is?`;
+    const reprompt = 'Say yes to start or no to quit.';
+    return responseBuilder
+      .speak(speechOutput)
+      .reprompt(reprompt)
+      .getResponse();
+  },
+};
+
+const ExitHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+
+    return request.type === 'IntentRequest'
+      && (request.intent.name === 'AMAZON.CancelIntent'
+        || request.intent.name === 'AMAZON.StopIntent');
+  },
+  handle(handlerInput) {
+    return handlerInput.responseBuilder
+      .speak('Thanks for playing!')
+      .getResponse();
+  },
+};
+
+const wuNameRetriever = (event) => {
     var firstName = event.firstName;
     var lastName = event.lastName;
 
@@ -36,3 +77,29 @@ exports.handler = async (event) => {
 
   });
 };
+
+const ErrorHandler = {
+  canHandle() {
+    return true;
+  },
+  handle(handlerInput, error) {
+    console.log(`Error handled: ${error.message}`);
+
+    return handlerInput.responseBuilder
+      .speak('Sorry, I can\'t understand the command. Please say again.')
+      .reprompt('Sorry, I can\'t understand the command. Please say again.')
+      .getResponse();
+  },
+};
+
+
+// Register request handlers
+const skillBuilder = Alexa.SkillBuilders.standard();
+
+exports.handler = skillBuilder
+  .addRequestHandlers(
+    LaunchRequest,
+    ExitHandler,
+  )
+  .addErrorHandlers(ErrorHandler)
+  .lambda();
